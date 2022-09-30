4e57d596949ceea4bf207d2306aedaf07379fbea
lautaro <lautaguzman21@hotmail.com>
2022-09-30 12:15:27 -0300

    operador ++

--- app.js
+++ app.js
@@ -3,17 +3,131 @@ const stock = document.getElementById("productos");
 const verCarrito = document.getElementById("verCarrito");
 
 const modalContainer = document.getElementById("modal-container");
+
+const listaCamisetas = [
+  {
+    id: 1,
+    nombre: "Argentina",
+    precio: 20000,
+    img: "imagenes/argentina/argentina titular.jpg",
+    cantidad: 1,
+  },
+
+  {
+    id: 2,
+    nombre: "Alemania",
+    precio: 15000,
+    img: "imagenes/alemania/alemania titular.jpg",
+    cantidad: 1,
+  },
+
+  {
+    id: 3,
+    nombre: "Belgica",
+    precio: 16000,
+    img: "imagenes/belgica/belgica titular.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 4,
+    nombre: "Brasil",
+    precio: 20000,
+    img: "imagenes/brasil/brasil titular.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 5,
+    nombre: "Ecuador",
+    precio: 12000,
+    img: "imagenes/ecuador/ecuador titular.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 15,
+    nombre: "Uruguay",
+    precio: 13000,
+    img: "imagenes/uruguay/uruguay titular.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 7,
+    nombre: "Ghana",
+    precio: 10000,
+    img: "imagenes/ghana/ghana titular.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 11,
+    nombre: "Mexico",
+    precio: 9000,
+    img: "imagenes/mexico/mexico titular.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 9,
+    nombre: "Japon",
+    precio: 13000,
+    img: "imagenes/japon/japon titular.jpg",
+    cantidad: 1,
+  },
+
+  {
+    id: 6,
+    nombre: "Espania",
+    precio: 16000,
+    img: "imagenes/españa/españa titular.jpg",
+    cantidad: 1,
+  },
+
+  {
+    id: 10,
+    nombre: "Marruecos",
+    precio: 8000,
+    img: "imagenes/marruecos/marruecos titular.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 12,
+    nombre: "Senegal",
+    precio: 8000,
+    img: "imagenes/senegal/senegal titular.jpg",
+    cantidad: 1,
+  },
+
+  /* {
+      id: 8,
+      nombre: "Inglaterra",
+      precio: 13000,
+      img: "imagenes/inglaterra/inglaterra titular.jpg",cantidad: 1,
+    },
+     
+      {
+        id: 13,
+        nombre: "Camiseta de Serbia",
+        precio: 10000,
+        img: "imagenes/serbia/serbia titular.jpg",cantidad: 1,
+      },
+      {
+        id: 14,
+        nombre: "Camiseta de Suiza",
+        precio: 9000,
+        img: "imagenes/suiza/suiza titular.jpg",cantidad: 1,
+      },*/
+];
+
 let carrito = [];
 
 listaCamisetas.forEach((camisetas) => {
   let content = document.createElement("div");
   content.className = "tarjeta";
-  content.innerHTML = `
-  <div class=""> 
-  <img src="${camisetas.img}">
-  <h3 class="text-capitalize fst-italic text-decoration-underline">${camisetas.nombre} </h3>
-  <p class="text-warning  fw-bold fs-4">${camisetas.precio}$</p>
-  </div> `;
+  content.innerHTML = ` 
+  <div class="card text-white bg-primary mb-3" style="width: 18rem;">
+  <img src="${camisetas.img}" class="card-img-top" alt="">
+  <div class="card-body">
+    <h5 class="card-title text-capitalize fst-italic text-decoration-underline">${camisetas.nombre}</h5>
+    <p class="card-text  fw-bold fs-4">${camisetas.precio}$</p>
+  </div>
+</div> `;
 
   stock.append(content);
 
@@ -24,53 +138,24 @@ listaCamisetas.forEach((camisetas) => {
   content.append(botonComprar);
 
   botonComprar.addEventListener("click", () => {
-    carrito.push({
-      id: camisetas.id,
-      nombre: camisetas.nombre,
-      precio: camisetas.precio,
-      img: camisetas.img,
-    });
+    const repetir = carrito.some(
+      (repeatProduct) => repeatProduct.id === camisetas.id
+    );
+    if (repetir) {
+      carrito.map((prod) => {
+        if (prod.id === camisetas.id) {
+          prod.cantidad++;
+        }
+      });
+    } else {
+      carrito.push({
+        id: camisetas.id,
+        nombre: camisetas.nombre,
+        precio: camisetas.precio,
+        img: camisetas.img,
+        cantidad: camisetas.cantidad,
+      });
+    }
     console.log(carrito);
   });
-});
-
-verCarrito.addEventListener("click", () => {
-  modalContainer.innerHTML = "";
-  modalContainer.style.display = "flex";
-  const modalHeader = document.createElement("div");
-  modalHeader.className = "modal-header";
-  modalHeader.innerHTML = `
-  <h1 class="modal-header-titulo">Tu Compra</h1>
-  
-  `;
-
-  modalContainer.append(modalHeader);
-
-  const modalButton = document.createElement("h1");
-  modalButton.innerText = "";
-  modalButton.className = "btn-close btn-close-white aria-label=Close";
-
-  modalButton.addEventListener("click", () => {
-    modalContainer.style.display = "none";
-  });
-
-  modalHeader.append(modalButton);
-
-  carrito.forEach((camisetas) => {
-    let carritoContent = document.createElement("div");
-    carritoContent.className = "modal-content";
-    carritoContent.innerHTML = `
-    <img src="${camisetas.img}">
-    <h3>${camisetas.nombre} </h3>
-    <p>$${camisetas.precio}</p>`;
-
-    modalContainer.append(carritoContent);
-  });
-
-  const total = carrito.reduce((acc, el) => acc + el.precio, 0);
-
-  const totalTodo = document.createElement("div");
-  totalTodo.className = "total-content";
-  totalTodo.innerHTML = `total hasta el momento$${total}`;
-  modalContainer.append(totalTodo);
 });
---
+++ carrito.js
@@ -0,0 +1,58 @@
+const pintarCarrito = () => {
+  modalContainer.innerHTML = "";
+  modalContainer.style.display = "flex";
+  const modalHeader = document.createElement("div");
+  modalHeader.className = "modal-header";
+  modalHeader.innerHTML = `
+    <h1 class="modal-header-titulo">Tu Compra</h1>
+    
+    `;
+
+  modalContainer.append(modalHeader);
+
+  const modalButton = document.createElement("h1");
+  modalButton.innerText = "";
+  modalButton.className = "btn-close btn-close-white aria-label=Close";
+
+  modalButton.addEventListener("click", () => {
+    modalContainer.style.display = "none";
+  });
+
+  modalHeader.append(modalButton);
+
+  carrito.forEach((camisetas) => {
+    let carritoContent = document.createElement("div");
+    carritoContent.className = "modal-content";
+    carritoContent.innerHTML = `
+      <img src="${camisetas.img}">
+      <h3>${camisetas.nombre} </h3>
+      <p>$${camisetas.precio}</p>
+      <p>cantidad: ${camisetas.cantidad}</p>`;
+
+    modalContainer.append(carritoContent);
+
+    let eliminar = document.createElement("span");
+    eliminar.innerText = "x";
+    eliminar.className = "delete-product";
+    carritoContent.append(eliminar);
+
+    eliminar.addEventListener("click", quitarProducto);
+  });
+
+  const total = carrito.reduce((acc, el) => acc + el.precio, 0);
+
+  const totalTodo = document.createElement("div");
+  totalTodo.className = "total-content";
+  totalTodo.innerHTML = `total hasta el momento$${total}`;
+  modalContainer.append(totalTodo);
+};
+verCarrito.addEventListener("click", pintarCarrito);
+
+const quitarProducto = () => {
+  const founId = carrito.find((element) => element.id);
+
+  carrito = carrito.filter((carritoId) => {
+    return carritoId !== founId;
+  });
+  pintarCarrito();
+};
